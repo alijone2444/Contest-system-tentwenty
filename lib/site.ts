ff9a355ca0e02505ecfd2f36bceae60e0a0d8977
Ali <alijone2444@gmail.com>
2026-07-03 10:50:15 +0500

    Set GitHub and contact links

--- lib/site.ts
+++ lib/site.ts
@@ -1,5 +1,5 @@
 export const site = {
   name: "Contest System",
-  github: process.env.NEXT_PUBLIC_GITHUB_URL || "https://github.com/your-username/contest-system",
-  email: process.env.NEXT_PUBLIC_CONTACT_EMAIL || "annassalman.98@gmail.com",
+  github: process.env.NEXT_PUBLIC_GITHUB_URL || "https://github.com/alijone2444/Contest-system-tentwenty",
+  email: process.env.NEXT_PUBLIC_CONTACT_EMAIL || "alijone2444@gmail.com",
 };
